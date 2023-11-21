ab1613d736d9f13d541eaece54befd38da8bca0c
K1lezsx <151659770+K1lezsx@users.noreply.github.com>
2023-11-21 11:10:10 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include("template/cabecera.php"); ?>
+    
+            <div class="jumbotron text-center">
+                <h1 class="display-3">Bienvenido a Shopping Quiles</h1>
+                <p class="lead">Esperamos que nuestros productos sean de tu agrado y te interese seguir comprando y ser parte de la comunidad </p>
+                <hr class="my-2">
+                <img src="img/SQuiles.jpeg">
+                <br> 
+                </p>
+            </div>
+
+<?php include("template/pie.php"); ?>
---
+++ nosotros.php
@@ -0,0 +1,11 @@
+<?php include("template/cabecera.php"); ?>
+
+    <div class="jumbotron">
+        <h1 class="display-3">Nosotros</h1>
+        <p class="lead">Somos una empresa dedicada a la venta de ropa para gimnasio y equipo deportivo 
+            <br> Siguenos en nuestras redes sociales para mas informacion y seguir comprando con nosotros
+        </p>
+        <hr class="my-2">
+    </div>
+
+<?php include("template/pie.php"); ?>
